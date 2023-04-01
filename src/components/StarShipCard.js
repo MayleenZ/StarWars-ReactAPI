0eba43cf912c88734269cfa7eb4c65c44bbb074f
Mayleen <mayleennyc@gmail.com>
2023-04-01 09:49:30 -0400

    starshipcss

--- src/components/StarShipCard.js
+++ src/components/StarShipCard.js
@@ -4,10 +4,12 @@ function StarShipCard(props) {
   return (
       <div className="starships">
         <h1>{starship.name}</h1>
+        <div id = "starship">
         <p>Crew: {starship.crew}</p>
         <p>Model: {starship.model}</p>
         <p>Passengers: {starship.passengers}</p>
         <p>Speed: {starship.max_atmosphering_speed}</p>
+        </div>
       </div>
   );
 }
